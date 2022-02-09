2c20935650bebc897256e8cc99bd000c3811261f
DavidAlvaradoSoftware <dalvaradosoftware@gmail.com>
2022-02-09 12:41:11 -0600

    flight doPost implemented

--- src/main/java/servlets/FlightsServlet.java
+++ src/main/java/servlets/FlightsServlet.java
@@ -1,5 +1,10 @@
 package servlets;
 
+import ORM.ORM;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import objects.Customers;
+import objects.Flights;
+import utils.DBcredentials;
 import utils.FileLogger;
 
 import javax.servlet.ServletException;
@@ -13,16 +18,35 @@ public class FlightsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        try {
-            //go back
-        } catch (Exception e) {
-            FileLogger.getFileLogger().log(e);
-        }
+//        ObjectMapper mapper = new ObjectMapper();
+//        Flights payload = mapper.readValue(req.getInputStream(), Flights.class);
+//        ORM orm = new ORM();
+//        DBcredentials cred = new DBcredentials();
+//        orm.connect(cred.getHostname(),
+//                cred.getPort(),
+//                cred.getDbname(),
+//                cred.getUsername(),
+//                cred.getPassword());
+//
+//        orm.ormEntry(payload,"insert");
+//        resp.setStatus(200);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        ObjectMapper mapper = new ObjectMapper();
+        Flights payload = mapper.readValue(req.getInputStream(), Flights.class);
+        ORM orm = new ORM();
+        DBcredentials cred = new DBcredentials();
+        orm.connect(cred.getHostname(),
+                cred.getPort(),
+                cred.getDbname(),
+                cred.getUsername(),
+                cred.getPassword());
+
+        orm.ormEntry(payload,"insert");
+        resp.setStatus(200);
     }
 
 }
