761fdd2c5f8bf55937c86cc506cd05bcc197d899
DavidAlvaradoSoftware <dalvaradosoftware@gmail.com>
2022-02-09 16:07:40 -0600

    debugging

--- src/main/java/servlets/TicketsServlet.java
+++ src/main/java/servlets/TicketsServlet.java
@@ -2,11 +2,8 @@ package servlets;
 
 import ORM.ORM;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import objects.Customers;
 import objects.Tickets;
 import utils.DBcredentials;
-import utils.FileLogger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
